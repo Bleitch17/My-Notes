5400de5a3b684a91f5e298ee3b8200d03dc4cc29
Ben Leitch <benleitch17@gmail.com>
2025-07-08 19:31:43 -0700

    Small update - make note blocks appear on top of canvas instead of taking up space alongside it.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { initCanvas } from './canvas.js';
 import { loadNotes } from './notes.js';
 
+// Entry point for the program.
+
 initCanvas();
 loadNotes();
--- src/notes.js
+++ src/notes.js
@@ -1,7 +1,7 @@
 import { marked } from 'marked';
 
 export function loadNotes() {
-  const container = document.getElementById('note-layer');
+  const container = document.getElementById('notes-container');
   const div = document.createElement('div');
   div.className = 'note-block';
   div.innerHTML = marked('# Hello from Markdown');
